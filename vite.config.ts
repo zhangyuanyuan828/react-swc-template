f892e5e198c56e8b69d538d9e88f21db0a2a188b
玲玲子 <zhangyuanyuan828@outlook.com>
2023-01-11 16:17:45 +0800

    feat: compress

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { promisify } from 'node:util'
+import { brotliCompress } from 'node:zlib'
+import compress from 'rollup-plugin-gzip'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    compress({
+      filter: /\.(js|mjs|cjs|json|css|html|xml|svg|wasm)$/i
+    }),
+    compress({
+      filter: /\.(js|mjs|cjs|json|css|html|xml|svg|wasm)$/i,
+      fileName: '.br',
+      customCompression: (content) => promisify(brotliCompress)(content)
+    })
+  ]
 })
