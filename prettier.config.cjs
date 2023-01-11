bc7f2de6284a60dc1d13c1217c120007d9136e39
玲玲子 <zhangyuanyuan828@outlook.com>
2023-01-11 15:27:37 +0800

    feat: initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+/**
+ * @type {import('prettier').Config}
+ */
+module.exports = {
+  bracketSameLine: true,
+  printWidth: 160,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  trailingComma: 'none',
+  plugins: ['prettier-plugin-organize-imports']
+}
